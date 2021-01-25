88dfe5b53f940f1d7dc86eae3fcb5dc02e79d812
Ringoer <ringoer@qq.com>
2021-01-25 15:55:06 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    导航栏 |
+    <router-link to="/">index</router-link> |
+    <router-link to="hello">hello</router-link>
+  </div>
+  <router-view />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
