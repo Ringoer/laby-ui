18a9f83bf3f3397722a155c56b691d630fe24f64
Ringoer <ringoer@qq.com>
2021-02-23 18:01:12 +0800

    修改了dialog第一个例子中的错误引用，并更新了table第一个例子

--- src/components/examples/Table/Table1.example.vue
+++ src/components/examples/Table/Table1.example.vue
@@ -16,7 +16,23 @@
           <laby-button @click="del(index)">删除</laby-button>
         </td>
       </tr>
+      <tr v-if="insertVisible">
+        <td v-for="key in keys" :key="key">
+          <input type="text" v-model="newData[key]" />
+        </td>
+        <td>
+          <laby-button @click="add">确定</laby-button>
+        </td>
+      </tr>
     </tbody>
+    <tfoot>
+      <tr>
+        <laby-button @click="insertVisible = !insertVisible">
+          <span v-if="insertVisible">取消添加</span>
+          <span v-else>添加记录</span>
+        </laby-button>
+      </tr>
+    </tfoot>
   </laby-table>
 </template>
 <script lang="ts">
@@ -42,7 +58,22 @@ export default {
     const del = (index) => {
       datas.value = datas.value.filter((item, i) => i != index);
     };
-    return { heads, keys, datas, del };
+    const insertVisible = ref(false);
+    const newData = ref(
+      keys.reduce((obj, item) => {
+        obj[item] = "";
+        return obj;
+      }, {})
+    );
+    const add = () => {
+      datas.value.push(newData.value);
+      newData.value = keys.reduce((obj, item) => {
+        obj[item] = "";
+        return obj;
+      }, {});
+      insertVisible.value = false;
+    };
+    return { insertVisible, heads, keys, datas, del, newData, add };
   },
 };
 </script>
